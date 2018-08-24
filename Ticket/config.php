35812f098841b3299c3557192adf0dc3320a537c
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-08-24 14:21:17 -0600

    Tickets Tile Bug

--- Ticket/config.php
+++ Ticket/config.php
@@ -69,4 +69,3 @@ if($strict_view > 0) {
 	$security['config'] = $tile_security['config'] = 0;
 }
 $db_config = explode(',',get_field_config($dbc, 'tickets_dashboard'));
-$ticketid = $_GET['edit'] > 0 ? $_GET['edit'] : 0;
