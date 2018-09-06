196b6e2f5753fbdd44f533703f286fc7cdcf5adb
khushboo <khushboo@invennico.com>
2018-09-06 11:22:30 +0530

    completed work "Each row of the operations tables should switch between silver and white"

--- Reports/report_operations_time_sheet.php
+++ Reports/report_operations_time_sheet.php
@@ -35,3 +35,17 @@ $value = $config['settings']['Choose Fields for Time Sheets Dashboard']; ?>
 	}
     include('../Timesheet/reporting_content.php'); ?>
 </div>
+<script>
+$('document').ready(function() {
+    var tables = $('table');
+
+    tables.map(function(idx, table) {
+        var rows = $(table).find('tbody > tr');
+        rows.map(function(idx, row){
+            if(idx%2 == 0) {
+                $(row).css('background-color', '#e6e6e6');
+            }
+        })
+    })
+})
+</script>
