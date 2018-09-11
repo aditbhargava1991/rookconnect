2632d22721d3e1e026407123a9024532e00f4eaf
jenish khunt <jenish.khunt1@lloydsbanking.com>
2018-09-11 13:51:00 +0100

    Task mandatory changes

--- db_data_jenish.php
+++ db_data_jenish.php
@@ -15,5 +15,9 @@ if(!mysqli_query($dbc, "ALTER TABLE `field_config_contacts` ADD `mandatory` BOOL
   echo "Error: ".mysqli_error($dbc)."<br />\n";
 }
 
+if(!mysqli_query($dbc, "CREATE TABLE task_dashboard_mandatory SELECT * FROM task_dashboard LIMIT 0")) {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
+
 echo "<br> ======Jenish's db changes Done======<br>";
 ?>
