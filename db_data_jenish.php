a41be379c0a07d1226b5017a50fa2708fffd12f5
jenish khunt <jenish.khunt1@lloydsbanking.com>
2018-09-16 19:28:28 +0100

    Adding security by the user

--- db_data_jenish.php
+++ db_data_jenish.php
@@ -10,6 +10,21 @@ echo "====== Jenish's db changes: ======\n";
 
 /****************** Adding indexing for Ticket tables *********************/
 
+if(!mysqli_query($dbc, "CREATE TABLE security_privileges_staff SELECT * FROM security_privileges LIMIT 0")) {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
+
+if(!mysqli_query($dbc, "ALTER TABLE `security_privileges_staff` ADD `staff` INT(15)")) {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
+
+if(!mysqli_query($dbc, "ALTER TABLE `security_privileges_staff` ADD PRIMARY KEY(`privilegesid`)")) {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
+
+if(!mysqli_query($dbc, "ALTER TABLE `security_privileges_staff` CHANGE `privilegesid` `privilegesid` INT(10) NOT NULL AUTO_INCREMENT") {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
 
 if(!mysqli_query($dbc, "ALTER TABLE `field_config_contacts` ADD `mandatory` BOOLEAN DEFAULT 0")) {
   echo "Error: ".mysqli_error($dbc)."<br />\n";
