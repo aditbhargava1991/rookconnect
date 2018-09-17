81ed51bad6a8478442ed2f7e86354752d226e925
Dayana <dayanapatel@freshfocusmedia.com>
2018-09-17 11:53:07 -0700

    8549

--- Sales/details_deliverable.php
+++ Sales/details_deliverable.php
@@ -8,7 +8,16 @@ if(empty($salesid)) {
 </script>
 <!-- Time Tracking -->
 <div class="accordion-block-details padded" id="deli">
-    <div class="accordion-block-details-heading"><h4>Time Line Countdown</h4></div>
+    <div class="accordion-block-details-heading"><h4>Deliverable</h4></div>
+
+        <?php
+         if(!empty($_GET['id'])) {
+            $sid = $_GET['id'];
+            $get_contact = mysqli_fetch_assoc(mysqli_query($dbc,"SELECT number_of_days, number_of_days_start_date FROM sales WHERE salesid='$sid'"));
+
+            $number_of_days = $get_contact ['number_of_days'];
+            $number_of_days_start_date = $get_contact ['number_of_days_start_date'];
+        ?>
 
         <div class="row set-row-height">
             <div class="col-xs-12 col-sm-4 gap-md-left-15">Total Number of Days:</div>
@@ -16,12 +25,23 @@ if(empty($salesid)) {
             <div class="clearfix"></div>
         </div>
 
-        <?php
-            $now = time(); // or your date as well
-            $your_date = strtotime($number_of_days_start_date);
-            $datediff = $now - $your_date;
+        <div class="clearfix"></div>
 
-            echo round($datediff / (60 * 60 * 24)).'/'.$number_of_days.' Days';
-        ?>
+        <?php
+        if($number_of_days > 0) { ?>
+        <div class="row set-row-height">
+            <div class="col-xs-12 col-sm-4 gap-md-left-15">Time Line Countdown:</div>
+            <div class="col-xs-12 col-sm-5">
+                <?php
+                    $now = time(); // or your date as well
+                    $your_date = strtotime($number_of_days_start_date);
+                    $datediff = $now - $your_date;
+
+                    echo round($datediff / (60 * 60 * 24)).'/'.$number_of_days.' Days';
+                ?>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <?php } ?>
 
 </div><!-- .accordion-block-details -->
