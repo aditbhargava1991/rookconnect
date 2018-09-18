12ee1236cc8a3e91cf959dcbdf7e81fdb5c548c4
khushboo <khushboo@invennico.com>
2018-09-19 00:30:37 +0530

    db query uploaded

--- db_data_invennico.php
+++ db_data_invennico.php
@@ -5,6 +5,6 @@
 echo "Invennico's DB Changes Done<br />\n";
 
 mysqli_query($dbc, "INSERT INTO `general_configuration` (`configid`, `name`, `value`, `calllog_schedule_status`) VALUES (NULL, 'summary_block_sort', '0', NULL)");
-
+ 
     echo "Invennico's DB Changes Done<br />\n";
 ?>
