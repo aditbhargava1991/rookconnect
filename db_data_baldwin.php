a393a91bc5c9bfb509c8b239844b5ede57517f16
Baldwin <baldwinyu@freshfocusmedia.com>
2018-09-19 12:43:05 -0600

    Calendar: Online Users
    # Conflicts:
    #	db_data_baldwin.php

--- db_data_baldwin.php
+++ db_data_baldwin.php
@@ -934,5 +934,23 @@
     }
     //2018-09-18 - Ticket #8755 - Sales Lead Info Gathering
 
+    //2018-09-19 - Ticket #8929 - Calendar Onlin Staff
+    if(!mysqli_query($dbc, "CREATE TABLE `calendar_last_active` (
+        `calendarlastactiveid` int(11) NOT NULL,
+        `type` varchar(500) NOT NULL,
+        `contactid` int(11) NOT NULL,
+        `last_active` datetime NOT NULL)")) {
+        echo "Error: ".mysqli_error($dbc)."<br />\n";
+    }
+    if(!mysqli_query($dbc, "ALTER TABLE `calendar_last_active`
+        ADD PRIMARY KEY (`calendarlastactiveid`)")) {
+        echo "Error: ".mysqli_error($dbc)."<br />\n";
+    }
+    if(!mysqli_query($dbc, "ALTER TABLE `calendar_last_active`
+        MODIFY `calendarlastactiveid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1")) {
+        echo "Error: ".mysqli_error($dbc)."<br />\n";
+    }
+    //2018-09-19 - Ticket #8929 - Calendar Onlin Staff
+
     echo "Baldwin's DB Changes Done<br />\n";
 ?>
