265b674f27db1e4a93c062d25528f6d2b585a98d
Dayana <dayanapatel@freshfocusmedia.com>
2018-09-21 11:17:49 -0700

    9207

--- Invoice/pay_receivables.php
+++ Invoice/pay_receivables.php
@@ -51,11 +51,9 @@ if (isset($_POST['submit_patient'])) {
             include ('pos_receivables_1.php');
 			break;
 		case 2:
-			//include('pos_invoice_2.php');
-            include ('pos_receivables_pink.php');
+            include ('pos_receivables_2.php');
 			break;
 		case 3:
-			//include('pos_invoice_3.php');
             include ('pos_receivables_3.php');
 			break;
 		case 4:
