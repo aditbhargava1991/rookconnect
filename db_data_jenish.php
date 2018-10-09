75abad1f32192628446c20154f744a49230998ea
jenish khunt <jenish.khunt1@lloydsbanking.com>
2018-10-09 09:11:35 +0100

    Database History and Backup and Reporting

--- db_data_jenish.php
+++ db_data_jenish.php
@@ -27,5 +27,9 @@ if(!mysqli_query($dbc, "CREATE TABLE `field_config_mandatory_contacts` LIKE `fie
   echo "Error: ".mysqli_error($dbc)."<br />\n";
 }
 
+if(!mysqli_query($dbc, "CREATE TABLE `db_backup_history` LIKE `checklist_history`")) {
+  echo "Error: ".mysqli_error($dbc)."<br />\n";
+}
+
 echo "<br> ======Jenish's db changes Done======<br>";
 ?>
