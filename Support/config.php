bf8a1f02420a1980c6989a6ada1af3d4633c5dd2
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-10-18 13:38:50 -0600

    Customer Support Styling Padding

--- Support/config.php
+++ Support/config.php
@@ -1,3 +1,3 @@
 <?php include_once('../include.php');
-// $dbc_support = mysqli_connect('localhost', 'ffm_rook_user', 'mIghtyLion!542', 'ffm_rook_db');
-$dbc_support = mysqli_connect('localhost', 'root', 'FreshFocus007', 'local_1_rook');
+$dbc_support = mysqli_connect('localhost', 'ffm_rook_user', 'mIghtyLion!542', 'ffm_rook_db');
+// $dbc_support = mysqli_connect('localhost', 'root', 'FreshFocus007', 'local_1_rook');
