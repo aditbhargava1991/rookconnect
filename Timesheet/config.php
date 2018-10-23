486415ed8a5d149d59733d37d7d1730894b431d7
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-10-23 15:22:01 -0600

    Time Sheet Corrections

--- Timesheet/config.php
+++ Timesheet/config.php
@@ -971,7 +971,7 @@ function get_pdf_options($dbc, $styling = '', $timesheet_tab = '') {
 		}
 	} else {
 		$value_config = explode(',',get_field_config($dbc, 'time_cards'));
-		if(!in_array('reg_hrs',$value_config) && !in_array('direct_hrs',$value_config) && !in_array('payable_hrs',$value_config) && !in_array('total_hrs',$value_config)) {
+		if(!in_array('total_hrs',$value_config) && !in_array('reg_hrs',$value_config) && !in_array('direct_hrs',$value_config) && !in_array('payable_hrs',$value_config) && !in_array('total_hrs',$value_config)) {
 		    $value_config = array_merge($value_config,['reg_hrs','extra_hrs','relief_hrs','sleep_hrs','sick_hrs','sick_used','stat_hrs','stat_used','vaca_hrs','vaca_used']);
 		}
 	}
