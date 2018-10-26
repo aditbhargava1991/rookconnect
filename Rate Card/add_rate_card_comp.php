7c4778a1274782d946a45ffa791ebe7729651e8a
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-10-26 08:56:43 -0600

    Vendor Compensation Details, Settings

--- Rate Card/add_rate_card_comp.php
+++ Rate Card/add_rate_card_comp.php
@@ -27,30 +27,36 @@ function remComp(img) {
         $comp_row = $comp_rates->fetch_assoc();
         do { ?>
             <div class="form-group compensation_detail">
-                <div class="col-sm-6" >
-                    <label class="col-sm-6 show-on-mob">Compensation for Item:</label>
-                    <select name="comp_item[]" class="chosen-select-deselect" data-placeholder="Select Compensation Item"><option />
-                        <option value="services" <?= $comp_row['item_type'] == 'services' ? 'selected' : '' ?>>Services</option>
-                        <option value="inventory" <?= $comp_row['item_type'] == 'inventory' ? 'selected' : '' ?>><?= INVENTORY_TILE ?></option>
-                        <option value="product" <?= $comp_row['item_type'] == 'product' ? 'selected' : '' ?>>Product</option>
-                        <option value="equipment" <?= $comp_row['item_type'] == 'equipment' ? 'selected' : '' ?>>Equipment</option>
-                        <option value="labour" <?= $comp_row['item_type'] == 'labour' ? 'selected' : '' ?>>Labour</option>
-                        <option value="material" <?= $comp_row['item_type'] == 'material' ? 'selected' : '' ?>>Material</option>
-                        <option value="vpl" <?= $comp_row['item_type'] == 'vpl' ? 'selected' : '' ?>>Vendor Pricelist</option>
-                        <option value="position" <?= $comp_row['item_type'] == 'position' ? 'selected' : '' ?>>Position</option>
-                        <option value="staff" <?= $comp_row['item_type'] == 'staff' ? 'selected' : '' ?>>Staff</option>
-                        <option value="misc" <?= $comp_row['item_type'] == 'misc' ? 'selected' : '' ?>>Miscellaneous Item</option>
-                    </select>
-                </div>
-                <div class="col-sm-4" >
-                    <label class="col-sm-4 show-on-mob">Compensation Rate (%):</label>
-                    <input name="comp_percent[]" type="number" min=0 max=100 step="any" class="form-control" value="<?= $comp_row['comp_percent'] ?>" />
-                </div>
-                <div class="col-sm-2">
+                <div class="pull-right" style="width:5em;">
                     <input type="hidden" name="comp_id[]" value="<?= $comp_row['id'] ?>">
                     <input type="hidden" name="comp_deleted[]" value="0">
                     <img class="cursor-hand inline-img pull-right" src="../img/remove.png" onclick="remComp(this);">
                     <img class="cursor-hand inline-img pull-right" src="../img/icons/ROOK-add-icon.png" onclick="addComp();">
+                </div>
+                <div class="scale-to-fill">
+                    <div class="col-sm-6">
+                        <label class="show-on-mob">Compensation for Item:</label>
+                        <select name="comp_item[]" class="chosen-select-deselect" data-placeholder="Select Compensation Item"><option />
+                            <option value="services" <?= $comp_row['item_type'] == 'services' ? 'selected' : '' ?>>Services</option>
+                            <option value="inventory" <?= $comp_row['item_type'] == 'inventory' ? 'selected' : '' ?>><?= INVENTORY_TILE ?></option>
+                            <option value="product" <?= $comp_row['item_type'] == 'product' ? 'selected' : '' ?>>Product</option>
+                            <option value="equipment" <?= $comp_row['item_type'] == 'equipment' ? 'selected' : '' ?>>Equipment</option>
+                            <option value="labour" <?= $comp_row['item_type'] == 'labour' ? 'selected' : '' ?>>Labour</option>
+                            <option value="material" <?= $comp_row['item_type'] == 'material' ? 'selected' : '' ?>>Material</option>
+                            <option value="vpl" <?= $comp_row['item_type'] == 'vpl' ? 'selected' : '' ?>>Vendor Pricelist</option>
+                            <option value="position" <?= $comp_row['item_type'] == 'position' ? 'selected' : '' ?>>Position</option>
+                            <option value="staff" <?= $comp_row['item_type'] == 'staff' ? 'selected' : '' ?>>Staff</option>
+                            <option value="misc" <?= $comp_row['item_type'] == 'misc' ? 'selected' : '' ?>>Miscellaneous Item</option>
+                            <option value="ticket" <?= $comp_row['item_type'] == 'ticket' ? 'selected' : '' ?>><?= TICKET_NOUN ?></option>
+                            <option value="day" <?= $comp_row['item_type'] == 'day' ? 'selected' : '' ?>>Daily</option>
+                            <option value="hour" <?= $comp_row['item_type'] == 'hour' ? 'selected' : '' ?>>Hourly</option>
+                            <option value="km" <?= $comp_row['item_type'] == 'km' ? 'selected' : '' ?>>Per KM</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-6">
+                        <label class="show-on-mob">Compensation Rate (%):</label>
+                        <input name="comp_percent[]" type="number" min=0 max=100 step="any" class="form-control" value="<?= $comp_row['comp_percent'] ?>" />
+                    </div>
                 </div>
                 <div class="clearfix"></div>
             </div>
